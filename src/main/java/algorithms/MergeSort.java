612defd628d4508c9c8f32658035849e6f3439dd
WENHUI CHEN <wenhui@WENHUIs-MacBook-Pro.local>
2018-12-04 15:36:45 -0500

    refactored structure

---
+++ src/main/java/algorithms/MergeSort.java
@@ -0,0 +1,13 @@
+package algorithms;
+
+import java.util.List;
+
+public class MergeSort {
+  public static  <E extends Comparable> void mergeSortGeneric(List<E> list) {
+
+  }
+
+  public static void mergeSortIntArray(int[] arr) {
+
+  }
+}
--- src/main/java/org/dragoncwh/algorithms/MergeSort.java
+++
@@ -1,5 +0,0 @@
-package org.dragoncwh.algorithms;
-
-public class MergeSort {
-
-}
